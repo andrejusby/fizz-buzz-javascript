7d99ac2f13f7570806131e6712b5f6f6e03061e8
andrejusby <andriusby@gmail.com>
2024-01-17 11:22:24 +0200

    update with some style

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 for(let i = 1; i < 350; i++) {
     let output = ''
+
+    if (i % 3 === 0) output += "Fizz"
+    if (i % 5 === 0) output += "Buzz"
+    if (i % 7 === 0) output += "Biff"
+    if (i % 9 === 0) output += "Fuzz"
+    if (i % 11 === 0) output += "Bizz"
+    console.log(output, i);
 }
